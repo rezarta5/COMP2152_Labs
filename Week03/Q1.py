e2d91fae7d5eb5d2c22d3d0d33d60ca8d99be23d
rezarta5 <rezartaqordja5@gmail.com>
2026-02-11 00:32:30 -0500

    add w04

--- Week03/Q1.py
+++ Week03/Q1.py
@@ -5,3 +5,4 @@ print(f"Sorted grades: {grades}") # Sorted grades: [78, 85, 88, 90, 92, 95]
 print(f"Highest grade: {grades[-1]}")
 print(f"Lowest grade: {grades[0]}")
 print(f"Total number of grades: {len(grades)}")
+
